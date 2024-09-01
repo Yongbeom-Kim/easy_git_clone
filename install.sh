6df96153986638337e7f252c8537dd21391f7d73
Kim Yongbeom <dernbu@gmail.com>
2024-09-01 14:42:19 +0800

    feat: Add path export to ~/.profile

--- install.sh
+++ install.sh
@@ -10,10 +10,15 @@ SCRIPTS_DIR=$BASE_DIR/scripts
 ln -s $SCRIPTS_DIR/* $BIN_DIR
 
 # Add BIN_DIR to PATH
-echo "export PATH=$BIN_DIR:\$PATH" >> ~/.bashrc
-if [ -f ~/.zshrc ]; then
-    echo "export PATH=$BIN_DIR:\$PATH" >> ~/.zshrc
+if [ -f ~/.profile ]; then 
+    echo "export PATH=$BIN_DIR:\$PATH" >> ~/.profile
+else
+    echo "export PATH=$BIN_DIR:\$PATH" >> ~/.bashrc
+    if [ -f ~/.zshrc ]; then
+        echo "export PATH=$BIN_DIR:\$PATH" >> ~/.zshrc
+    fi
 fi
+
 export PATH=$BIN_DIR:$PATH
 
 
